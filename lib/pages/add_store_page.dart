f90272d9dd80736aae3276fb0783595e6fcfa02d
Swagnik02 <swagnik1234@gmail.com>
2025-04-29 02:24:33 +0530

    added workflow

--- lib/pages/add_store_page.dart
+++ lib/pages/add_store_page.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:uuid/uuid.dart';
 
 class AddStorePage extends StatefulWidget {
   const AddStorePage({super.key});
